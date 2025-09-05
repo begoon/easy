3556de74ac3501e60fdc4ee3df49c1023ac9e0fa
Alexander Demin <alexander@demin.ws>
2025-09-05 12:25:27 +0100

    fix quote again

--- tests/quote/x/test.c
+++ tests/quote/x/test.c
@@ -4,9 +4,13 @@ int main()
 {
     strcpy(s.data, "\"");
     output(1, s);
+    output(1, "<1>");
     output(1, "");
+    output(1, "<2>");
     output(1, "\"");
+    output(1, "<3>");
     output(1, s);
+    output(1, "<4>");
     output(1, "...");
     exit(0);
 }
